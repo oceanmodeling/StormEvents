c634102ca5e249ffa14b3d10772bfd9a8cd33ea2
William Pringle <wpringle@anl.gov>
2024-06-24 13:04:12 -0500

    correctly handling nans

--- stormevents/nhc/track.py
+++ stormevents/nhc/track.py
@@ -1353,8 +1353,8 @@ def correct_ofcl_based_on_carq_n_hollandb(
             ):
                 continue
             # make sure rolling rmw is not larger than the maximum radii of the strongest isotach
-            max_isotach_radii = isotach_radii.loc[valid_index].values[-1].max()
-            if rmw < max_isotach_radii:
+            max_isotach_radii = isotach_radii.loc[valid_index].iloc[-1].max()
+            if rmw < max_isotach_radii or numpy.isnan(max_isotach_radii):
                 forecast.loc[valid_index, "radius_of_maximum_winds"] = rmw
 
         # fill OFCL background pressure with the first entry from 0-hr CARQ background pressure (at sea level)
