3987c0c9b957ff020cf919c0e4de946aa3169d97
Soroosh Mani <77082694+SorooshMani-NOAA@users.noreply.github.com>
2026-05-05 08:22:30 -0400

    Bugfix (#121)
    * Update USGS refs
    
    * Update test files added deck A for OFCL test
    
    * Always add first time to convex hull

--- stormevents/nhc/track.py
+++ stormevents/nhc/track.py
@@ -972,6 +972,7 @@ class VortexTrack:
             for track_start_time, track_isotachs in advisory_isotachs.items():
                 convex_hulls = []
                 isotach_times = list(track_isotachs)
+                convex_hulls.append(track_isotachs[isotach_times[0]])
                 for index in range(len(isotach_times) - 1):
                     convex_hulls.append(
                         ops.unary_union(
